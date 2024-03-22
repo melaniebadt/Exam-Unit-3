9f5d20ad330a98e74304eb43d0b57d44207d92af
Melanie Badt <melanie@Melanies-MacBook-Air.local>
2024-03-22 17:12:07 +0100

    Task 2 complete

--- Task2.cs
+++ Task2.cs
@@ -5,15 +5,12 @@ public class FlattenThoseNumbers
 {
     public void Execution()
     {
-
         string jsonString = File.ReadAllText("arrays.json");
         JsonElement jsonElement = JsonSerializer.Deserialize<JsonElement>(jsonString);
         List<int> flattenedArray = new List<int>();
         FlattenDynamicObject(jsonElement, flattenedArray);
         Console.WriteLine("🌱Flatten those Numbers🌱");
         Console.WriteLine("[{0}]", string.Join(",", flattenedArray));
-
-
     }
     private static void FlattenDynamicObject(JsonElement jsonObject, List<int> flattenedList)
     {
